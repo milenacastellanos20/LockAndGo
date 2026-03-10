6012aa1fbc20449641476f91bc162ec1ee403dbc
Larents <daniels.dants@gmail.com>
2026-03-10 20:42:11 +0100

    metodo lanzarPantallaBloqueo (y clase temporal)

--- app/src/main/java/com/dam/lockgo/services/AppBlockingService.kt
+++ app/src/main/java/com/dam/lockgo/services/AppBlockingService.kt
@@ -1,8 +1,8 @@
 package com.dam.lockgo.services
 
 import android.accessibilityservice.AccessibilityService
-import android.view.accessibility.AccessibilityEvent
 import android.content.Intent
+import android.view.accessibility.AccessibilityEvent
 import android.util.Log
 import kotlin.jvm.java
 
@@ -24,6 +24,11 @@ class AppBlockingService : AccessibilityService() {
 
             Log.d("LockAndGo", "App en primer plano: $packageName")
 
+            //esta en la lista negra y no ha cumplido los pasos ? Bloqueo por gordaaaaaaaaaaa ADELGAZAAA
+            if (bloquearApp(packageName)) {
+                lanzarPantallaBloqueo(packageName)
+            }
+
         }
     }
 
@@ -32,8 +37,31 @@ class AppBlockingService : AccessibilityService() {
         Log.d("LockAndGo", "Servicio de accesibilidad interrumpido.")
     }
 
+    private fun bloquearApp(packageName: String): Boolean {
 
+        //habra que pasarle la base de datos( room)
+        val appsBloqueadas = listOf("com.whatsapp", "com.instagram.android")
+        val pasosCompletados = false; // viene de Datos/sensores
 
+        return appsBloqueadas.contains(packageName) && !pasosCompletados
+    }
 
+    private fun lanzarPantallaBloqueo(blockedPackage: String) {
+        //lanzamos la pantalla de jetpack compose que bloquea el acceso
+        val intent = Intent(this, BlockingActivity::class.java).apply {
+            // Estos flags para que la pantalla de bloqueo se ponga por encima
+            addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK or //inicia la actividad en una nueva tarea
+
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK or //borra la instancia previa (evita que el usuario vuelva a ella)
+
+                        Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS or //excluye la actividad de las recientes (que no aparezca jiji)
+
+                        Intent.FLAG_ACTIVITY_NO_ANIMATION //quita la animación de inicio
+            )
+            putExtra("BLOCKED_PACKAGE", blockedPackage)
+        }
+        startActivity(intent)
+    }
 
 }
---
+++ app/src/main/java/com/dam/lockgo/services/BlockingActivity.kt
@@ -0,0 +1,4 @@
+package com.dam.lockgo.services
+
+class BlockingActivity {
+}
