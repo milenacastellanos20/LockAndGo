01bfeb247e1ebe8b810a81d4fe05919e891bee1b
K1ng-Yike567 <hugaroplay@gmail.com>
2026-03-25 17:10:06 +0100

    Lógica conectividad bluetooh implementada

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 
 dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/dam/lockgo/ui/screens/StartActivityScreen.kt
+++ app/src/main/java/com/dam/lockgo/ui/screens/StartActivityScreen.kt
@@ -1,5 +1,6 @@
 package com.dam.lockgo.ui.screens
 
+import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +13,7 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,16 +21,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.android.gms.wearable.Wearable
 
 @Preview
 @Composable
 fun StartActivityScreen(
     onBack: () -> Unit = {}
 ) {
+
+    val context = LocalContext.current
+    //Variables para el TextField
+    var text by remember { mutableStateOf("") }
+    val maxChars = 5
+
+    //Variable para el botón
+    var isPasos by remember { mutableStateOf(true) }
 
     Scaffold(
         topBar = { TopBarComponent(onBackClick = { onBack() }) },
@@ -41,11 +54,17 @@ fun StartActivityScreen(
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                TextFieldComponent()
+
+                TextFieldComponent(text = text, maxChars = maxChars, onValueChange = { text = it })
 
                 Spacer(modifier = Modifier.size(20.dp))
 
-                StartActivityButton()
+                StartActivityButton(text, hayPasos = { isPasos = it }, context)
+
+                if (!isPasos) {
+                    SinPasosAviso()
+                }
+
             }
         }
 
@@ -53,19 +72,15 @@ fun StartActivityScreen(
     }
 
 }
-
-
 @Composable
-fun TextFieldComponent() {
-    val maxChars = 5
-    var text by remember { mutableStateOf("") }
+fun TextFieldComponent(text: String, maxChars: Int,onValueChange: (String) -> Unit) {
 
     TextField(
         value = text,
         onValueChange = { newText ->
 
             if (newText.length <= maxChars && newText.all { it.isDigit() }) {
-                text = newText
+                onValueChange(newText)
             }
 
         },
@@ -76,15 +91,52 @@ fun TextFieldComponent() {
 }
 
 @Composable
-fun StartActivityButton() {
+fun StartActivityButton(pasos: String, hayPasos: (Boolean) -> Unit, context: Context) {
 
     Button(
         onClick = {
-
+            iniciarActividad(pasos, hayPasos, context = context)
         }
     ) {
         Text(text = "Comenzar actividad")
     }
 
+}
+
+@Composable
+fun SinPasosAviso() {
+
+    Text(
+        text = "¡No has introducido la meta de pasos!",
+        color = Color.Red
+    )
+
+}
+
+fun iniciarActividad(pasos: String, hayPasos: (Boolean) -> Unit, context: Context) {
+
+    if (pasos.isEmpty()) {
+        hayPasos(false)
+        return
+    }
+
+    hayPasos(true)
+
+    try {
+        //Lógica de enviar al reloj la meta de pasos para comenzar la actividad
+        val messageClient = Wearable.getMessageClient(context)
+
+        Wearable.getNodeClient(context).connectedNodes.addOnSuccessListener { nodes ->
+            for (node in nodes) {
+                messageClient.sendMessage(node.id,
+                    "/start_activity",
+                    pasos.toByteArray())
+            }
+        }
+
+
+    }catch (e: Exception) {
+        e.printStackTrace()
+    }
 
 }
