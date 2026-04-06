6aaa34e86823532870255be89b6e3b25c5439078
milenacastellanos20 <milenacastellanos3940@gmail.com>
2026-04-06 20:28:13 +0200

    Revert "Modulo de gamificación"

--- app/src/main/java/com/dam/lockgo/domain/model/RewardWallet.kt
+++
@@ -1,6 +0,0 @@
-package com.dam.lockgo.domain.model
-
-data class RewardWallet(
-    val coins: Int,
-    val completedObjectives: Int
-)
--- app/src/main/java/com/dam/lockgo/presentation/rewards/RewardUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.dam.lockgo.presentation.rewards
-
-data class RewardUiState(
-    val coins: Int = 0,
-    val completedObjectives: Int = 0,
-    val message: String = ""
-)
